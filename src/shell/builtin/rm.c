683bff64fa493e8a705c43987ec10b947f4f1aca
Ruohuang Zheng <zhengruohuang@gmail.com>
2016-11-11 10:26:12 -0500

    Shell supports rm

---
+++ src/shell/builtin/rm.c
@@ -0,0 +1,34 @@
+#include "common/include/data.h"
+#include "common/include/errno.h"
+#include "klibc/include/stdio.h"
+#include "klibc/include/stdlib.h"
+#include "klibc/include/sys.h"
+#include "shell/include/shell.h"
+
+
+static int do_rm(char *name)
+{
+    unsigned long id = open_path(name, 0);
+    kprintf("Open: %p\n", id);
+    
+    int err = kapi_urs_remove(id);
+    return err;
+}
+
+int rm(int argc, char **argv)
+{
+    int err = EOK;
+    
+    if (argc > 1) {
+        int i;
+        
+        for (i = 1; i < argc; i++) {
+            err = do_rm(argv[i]);
+            if (err != EOK) {
+                break;
+            }
+        }
+    }
+    
+    return err;
+}
